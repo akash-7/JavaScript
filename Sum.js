ec591aa152e3a7ae182734f70c454a99b0fdea64
Akash-madhavan <ash.mec16@gmail.com>
2021-05-22 11:51:52 +0530

    Sum

--- Sum.js
+++ Sum.js
@@ -1,6 +1,9 @@
 const n1 = 5;
 const n2 = 7;
-
 const sum = n1 + n2;
 
+const n3 = 9, n4 = 21;
+const sum2 = n3 + n4;
+
 console.log('The sum of ' +n1+ ' and ' +n2+ ' is ' +sum);
+console.log("The sum of " +n3+ " and " +n4+ ' is ' +sum2);
